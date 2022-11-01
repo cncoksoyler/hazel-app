0e6c4354b2188e7478c0f5eaf4ce3d11832a314f
Can Çoksöyler <cncoksoyler@MSI.localdomain>
2022-11-01 23:13:57 +0100

    Tailwind implemented

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import './registerServiceWorker'
 import store from './store'
 import router from './router'
+import './assets/tailwind.css'
 
 /* eslint-disable */
 createApp(App).use(router).use(store).mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,6 +9,8 @@
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 
+// import '../assets/tailwind.css';
+import '@/assets/tailwind.css'
 export default {
   name: 'HomeView',
   components: {
